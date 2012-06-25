52ee59d3a46cf0ac5653b5cbbbdf264446c48e6e
Masaya Konishi <konishi@office-ore.net>
2012-06-26 00:09:32 +0900

    最初のサンプルのcookbookとrecipesを作ってみた

---
+++ cookbooks/sample/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: sample
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+template "/tmp/sl001.txt" do
+  source "sl001.txt.erb"
+  mode 0644
+end
+
